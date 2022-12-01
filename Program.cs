adecf25e92f8a546cb41d236959be52aa58d84e9
Aleksandr <aleksandr.evlanicov@gmail.com>
2022-12-01 13:59:24 +0300

    addprogram

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("введите первое число " );
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("введите второе число " );
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if(number1 > number2){
+    Console.WriteLine("1 число больше чем 2 число");
+}
+else
+{
+    Console.WriteLine("2 число больше чем 1 число");
+}
+
